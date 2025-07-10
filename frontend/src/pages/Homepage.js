bccea0218544008a2f519f0517492e13fad151b3
karresarayu <sarayukarre@gmail.com>
2025-07-10 20:16:41 +0530

    commit

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -21,11 +21,11 @@ const Homepage = () => {
                             <br />
                             System
                         </StyledTitle>
-                        <StyledText>
+                        {/* <StyledText>
                             Streamline school management, class organization, and add students and faculty.
                             Seamlessly track attendance, assess performance, and provide feedback.
                             Access records, view marks, and communicate effortlessly.
-                        </StyledText>
+                        </StyledText> */}
                         <StyledBox>
                             <StyledLink to="/choose">
                                 <LightPurpleButton variant="contained" fullWidth>
