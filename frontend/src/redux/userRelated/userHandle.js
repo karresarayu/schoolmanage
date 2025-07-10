0232748c86c5c6fea3a584f886f51719fba50dc6
karresarayu <sarayukarre@gmail.com>
2025-07-10 17:18:50 +0530

    commit

--- frontend/src/redux/userRelated/userHandle.js
+++ frontend/src/redux/userRelated/userHandle.js
@@ -75,7 +75,7 @@ export const deleteUser = (id, address) => async (dispatch) => {
     dispatch(getRequest());
 
     try {
-        const result = await axios.delete(`${process.env.REACT_APP_BASE_URL}/${address}/${id}`);
+        const result = await axios.delete(`${REACT_APP_BASE_URL}/${address}/${id}`);
         if (result.data.message) {
             dispatch(getFailed(result.data.message));
         } else {
