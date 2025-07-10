a941acabc579016c3646882fa513c2b2a06636ff
karresarayu <sarayukarre@gmail.com>
2025-07-10 20:19:23 +0530

    commit

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -26,6 +26,8 @@ const Homepage = () => {
                             Seamlessly track attendance, assess performance, and provide feedback.
                             Access records, view marks, and communicate effortlessly.
                         </StyledText> */}
+
+
                         <StyledBox>
                             <StyledLink to="/choose">
                                 <LightPurpleButton variant="contained" fullWidth>
